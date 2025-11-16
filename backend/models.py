1306fc8f4d6157187263c4a55b37ad371b695f0a
harsh42021 <harsh42021@gmail.com>
2025-11-16 01:32:00 -0500

    Add SQLAlchemy models for Retailer and Product

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,39 @@
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Retailer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    active = db.Column(db.Boolean, default=True)
+
+class Build(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+
+class Part(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    build_id = db.Column(db.Integer, db.ForeignKey('build.id'))
+    category = db.Column(db.String(50))
+    oem = db.Column(db.String(50))
+    label = db.Column(db.String(100), nullable=True)
+
+class ProductUrl(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    oem = db.Column(db.String(50))
+    retailer_id = db.Column(db.Integer, db.ForeignKey('retailer.id'))
+    url = db.Column(db.String(500))
+
+class PriceHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    oem = db.Column(db.String(50))
+    retailer_id = db.Column(db.Integer, db.ForeignKey('retailer.id'))
+    price = db.Column(db.Float)
+    currency = db.Column(db.String(10))
+    timestamp = db.Column(db.DateTime, server_default=db.func.now())
+
+class NotificationSettings(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pushbullet_token = db.Column(db.String(200))
+    enable = db.Column(db.Boolean, default=False)
